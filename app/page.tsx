a4ad8481cc489e49146f71b27798f5533b72f18d
corsini2025 <alessandroferrise2017@gmail.com>
2025-08-09 13:53:12 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,1 @@
-import from React from "react";
-import "./globals.css";
-
-export default function Page() {
-  return (
-    <main className="main">
-      <h1>Corsini Art Gallery</h1>
-    </main>
-  );
-}
 
